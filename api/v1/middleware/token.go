cb52cb1acabbeb5e9cc2ff72b291b2f54256a7e3
Okeyja Teung <dengyijian163@163.com>
2022-04-02 09:49:07 +0800

    fix: HTTP response error message format.

--- api/v1/middleware/token.go
+++ api/v1/middleware/token.go
@@ -11,13 +11,13 @@ func Token() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		tokenValueFromHeader := c.Request.Header.Get(util.HeaderToken)
 		if tokenValueFromHeader == "" {
-			util.ErrorPack(c).WithMessage("HTTP header \"%v\" is required for this request.", util.HeaderToken).WithHttpResponseCode(http.StatusBadRequest).Responds()
+			util.ErrorPack(c).WithMessage("HTTP header `%v` is required for this request.", util.HeaderToken).WithHttpResponseCode(http.StatusBadRequest).Responds()
 			c.Abort()
 			return
 		}
 		parts := strings.SplitN(tokenValueFromHeader, " ", 2)
 		if !(len(parts) == 2 && parts[0] == "Bearer") {
-			util.ErrorPack(c).WithMessage("HTTP header \"%v\" is in illegal format.", util.HeaderToken).WithHttpResponseCode(http.StatusBadRequest).Responds()
+			util.ErrorPack(c).WithMessage("HTTP header `%v` is in illegal format.", util.HeaderToken).WithHttpResponseCode(http.StatusBadRequest).Responds()
 			c.Abort()
 			return
 		}
@@ -31,13 +31,13 @@ func Signature() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		signatureValueFromHeader := c.Request.Header.Get(util.HeaderToken)
 		if signatureValueFromHeader == "" {
-			util.ErrorPack(c).WithMessage("HTTP header \"%v\" is required for this request.", util.HeaderToken).WithHttpResponseCode(http.StatusBadRequest).Responds()
+			util.ErrorPack(c).WithMessage("HTTP header `%v` is required for this request.", util.HeaderToken).WithHttpResponseCode(http.StatusBadRequest).Responds()
 			c.Abort()
 			return
 		}
 		timestampValueFromHeader := c.Request.Header.Get(util.HeaderTimestamp)
 		if timestampValueFromHeader == "" {
-			util.ErrorPack(c).WithMessage("HTTP header \"%v\" is required for this request.", util.HeaderTimestamp).WithHttpResponseCode(http.StatusBadRequest).Responds()
+			util.ErrorPack(c).WithMessage("HTTP header `%v` is required for this request.", util.HeaderTimestamp).WithHttpResponseCode(http.StatusBadRequest).Responds()
 			c.Abort()
 			return
 		}
