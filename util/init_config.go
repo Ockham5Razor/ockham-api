38fc2603d09e66f31009a758c7bb1254956f1bfc
Teung <dengyijian163@163.com>
2023-08-31 13:35:01 +0800

    feature: generate default configure file.

--- util/init_config.go
+++ util/init_config.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"log"
 	"os"
+	"path/filepath"
 )
 
 func InitConfig(cfgFile string) {
@@ -19,21 +21,38 @@ func InitConfig(cfgFile string) {
 		viper.AddConfigPath(home + "/.ockham-api/")
 		viper.SetConfigName("configs")
 		viper.SetConfigType("yaml")
+		cfgFile = filepath.Join(home, ".ockham-api", "configs.yaml")
 	}
 
 	viper.AutomaticEnv()
 
 	if err := viper.ReadInConfig(); err != nil {
-		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			// Config file not found; ignore error if desired
-			fmt.Println("config file not found.")
-		} else {
-			// Config file was found but another error was produced
-			fmt.Println("config file read other error: ", err.Error())
+		fmt.Printf("Config file not found or read error, should we create or override config file: `%v`? (Y/n): ", cfgFile)
+		var shouldOverride = "Y"
+		_, scanInputErr := fmt.Scanln(&shouldOverride)
+		if scanInputErr != nil {
+			log.Println("Invalid input, using default value [Y].")
 		}
-		os.Exit(2)
+		if shouldOverride == "Y" || shouldOverride == "y" || shouldOverride == "" {
+			fmt.Printf("Creating or overrideing config file: `%v`...\n", cfgFile)
+			dir := filepath.Dir(cfgFile)
+			mkdirErr := os.MkdirAll(dir, 0755)
+			if mkdirErr != nil {
+				fmt.Printf("Error creating or overrideing config file: `%v`! Error is: %v\n", cfgFile, mkdirErr)
+				os.Exit(3)
+			}
+			f, createFileErr := os.Create(cfgFile)
+			defer f.Close()
+			if createFileErr != nil {
+				fmt.Printf("Error creating or overrideing config file: `%v`! Error is: %v\n", cfgFile, createFileErr)
+				os.Exit(4)
+			}
+		} else {
+			fmt.Printf("Canceled creating or overrideing config file: `%v`.\n", cfgFile)
+			os.Exit(2)
+		}
 	} else {
-		fmt.Println("using config file: ", viper.ConfigFileUsed())
+		fmt.Println("Using config file: ", cfgFile)
 	}
 	// Config file found and successfully parsed
 }
